4c19bbec2f8d7259c151e1c6d9bf21e6a738a4c8
YoshifumiSato <yoshifumi0521@gmail.com>
2012-11-06 01:50:10 +0900

    大幅修正

--- public/client.js
+++ public/client.js
@@ -2,15 +2,21 @@ $(function() {
   var socket = io.connect();
 
   socket.on('connect', function() {
-    console.log('connected');
+   
+   console.log('クライアントが接続');
 
-    //socket.emit('msg send', 'message from client');
+   
+   //socket.emit('msg send', 'message from client');
 
     //socket.on('msg push', function(data) {
-    //  console.log(data); // 'message from server'
+
+      //console.log("クライアントが"); // 'message from server'
+    
+    
     //});
-  
-  
-  
-  });
+   
+   
+   
+   
+   });
 });
