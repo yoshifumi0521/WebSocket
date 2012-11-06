09c5aebc46224924f1128633c7eeb07af21d23c6
YoshifumiSato <yoshifumi0521@gmail.com>
2012-11-07 03:47:56 +0900

    コメントを追加

--- public/chat.js
+++ public/chat.js
@@ -3,21 +3,29 @@ $(function() {
     , $posts   = $('ul#posts')
     , $message = $('input#message')
     ;
-
+   
+  //"login"イベントをつくって、サーバーからきたらする処理。
   socket.on('login', function(data) {
+    //ソケットIDを表示させる。
     var $li = $('<li>').text(data + ' joined.');
     $posts.prepend($li);
   });
 
+  //"POST"イベントを用意して、サーバーからきたらする処理。
   socket.on('post', function(data) {
+    //表示する。
     var $li = $('<li>').text(data.id + ' say: ' + data.post);
     $posts.prepend($li);
   });
 
+  //入力して、ボタンをクリックしたらする処理。
   $('input#update').on('click', function(e) {
+
     var message = $message.val();
     if (message.length === 0) return;
+    //サーバーに"post"として送る。
     socket.emit('post', message);
+    //入力内容を空にする。
     $message.val('');
   });
 });
--- server.js
+++ server.js
@@ -14,12 +14,19 @@ var io = require('socket.io')
   , io = io.listen(server)
   ;
 
+//接続されたらする処理
 io.sockets.on('connection', function(socket) {
   
   console.log("サーバーの接続");
-
+  //"login"イベントで、ソケットのIDを送る。
   io.sockets.emit('login', socket.id);
+
+  //"post"イベントを用意して、クライアントからリクエストがきたらする処理。
   socket.on('post', function(data) {
+    //クライアントに送る。
     io.sockets.emit('post', { id: socket.id, post: data });
   });
+
+
+
 });
